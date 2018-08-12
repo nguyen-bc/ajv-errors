c2b15f75f47a49c9405fbfcf7cfaf7d672cd3a78
Bui Chi Nguyen <administrator@neniss.com>
2018-08-12 16:28:20 +0700

    up to 1.0.1

--- test/lib/index.spec.js
+++ test/lib/index.spec.js
@@ -30,6 +30,16 @@ test('field name is value from dataPath if params have not missingProperty', t =
   t.is(typeof errors.aField, 'string');
 });
 
+test('field name will be created by dataPath and missingProperty', t => {
+  var errors = ajvErrors([{
+    params: { missingProperty: 'aField' },
+    dataPath: '.items[0]',
+    message: 'shoud provide a required property \\"aField\\"'
+  }]);
+  t.is(Object.keys(errors).length, 1);
+  t.is(typeof errors['items[0].aField'], 'string');
+});
+
 test('return all errors messages', t => {
   var errors = ajvErrors([{
     dataPath: '.aField1',
